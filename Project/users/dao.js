20b45fa9f223777d61e1eda88a21d92ce9dcb6fd
Tinashe Mazaiwana <98105426+Lemonflavur@users.noreply.github.com>
2023-12-11 18:22:23 -0500

    a6 Mongo fa23

--- Project/users/dao.js
+++ Project/users/dao.js
@@ -1,14 +1,22 @@
 import model from "./model.js";
 export const createUser = (user) => model.create(user);
+
 export const findAllUsers = () => model.find();
-export const findUserById = (userId) => model.findById(userId);
+
+export const findUserById = (userId) => model.findById({_id: userId});
+
 export const findUserByUsername = (username) =>
     model.findOne({ username: username });
+
 export const findUserByCredentials = (username, password) =>
-    model.findOne({ username, password });
-export const updateUser = (userId, user) =>
-    model.updateOne({ _id: userId }, { $set: user });
-export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+    model.findOne({ username: username, password: password });
+
+export const findUserByFirstName = (firstName) =>
+    model.findOne({ firstName: firstName });
 
 export const updateUser = (userId, user) =>
     model.updateOne({ _id: userId }, { $set: user });
+
+
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+
